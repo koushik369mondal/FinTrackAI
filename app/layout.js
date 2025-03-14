c5b1c9a88e44a00f1b1c19e3ed09bad92b1b0ebd
Kaushik Mandal <koushik369mondal@gmail.com>
2025-03-14 23:56:07 +0530

    set header

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/header";
 
 const inter  = Inter({subsets: ["latin"] });
 
@@ -13,6 +14,8 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={`${inter.className}`}>
         {/*header*/}
+        <Header/>
+
         <main className="min-h-screen">
           {children}
         </main>

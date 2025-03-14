99f634ec1ddd74d95f1a7d232de7f42b2ca3ee1c
Kaushik Mandal <koushik369mondal@gmail.com>
2025-03-15 00:40:30 +0530

    user authenticatior -signin , signout

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter  = Inter({subsets: ["latin"] });
 
@@ -11,6 +12,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body className={`${inter.className}`}>
         {/*header*/}
@@ -27,6 +29,7 @@ export default function RootLayout({ children }) {
         </footer>
       </body>
     </html>
+    </ClerkProvider>
   );
 }
 

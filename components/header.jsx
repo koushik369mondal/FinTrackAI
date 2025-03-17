e8a0216722e94ba604f05b5d2a9efb5cf1896904
Kaushik Mandal <koushik369mondal@gmail.com>
2025-03-17 10:23:14 +0530

    set sign-in and sign-up to center

--- components/header.jsx
+++ components/header.jsx
@@ -2,7 +2,7 @@ import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
 const Header = () => {
     return (
-        <div>
+        <div className="fixed top-0">
             <SignedOut>
                 <SignInButton />
             </SignedOut>

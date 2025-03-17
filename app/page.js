be973ba221be2be60aa795139ad24b53120a8e16
Kaushik Mandal <koushik369mondal@gmail.com>
2025-03-17 11:39:47 +0530

    setup dashboard

--- app/page.js
+++ app/page.js
@@ -1,5 +1,11 @@
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return <Button variant="destructive">Subscribe to Roadside Coder</Button>;
+  return (
+    <div className="mt-40">
+        <Button variant="destructive">Subscribe to Roadside Coder</Button>;
+    </div>
+  );
+  
+
 }

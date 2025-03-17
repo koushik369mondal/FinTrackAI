9f63b0556b028e11eb4a76be2625c3d6f691c9f6
Kaushik Mandal <koushik369mondal@gmail.com>
2025-03-17 11:04:56 +0530

    download logo and set it

--- components/header.jsx
+++ components/header.jsx
@@ -1,8 +1,21 @@
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
 
 const Header = () => {
     return (
         <div className="fixed top-0">
+            <nav>
+                <Link href="/">
+                <Image src={"/logo.png"}
+                alt="FinTrackAi Logo"
+                height={60}
+                width={200}
+                className="h-12 w-auto object-contain"
+                />
+                </Link>
+            </nav>
+
             <SignedOut>
                 <SignInButton />
             </SignedOut>

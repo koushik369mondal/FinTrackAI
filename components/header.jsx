6024ba60288109ede7001e058f16fb22faa749d9
Kaushik Mandal <koushik369mondal@gmail.com>
2025-03-23 20:02:55 +0530

    feat: add 'Add Transaction' button to header for signed-in users

--- components/header.jsx
+++ components/header.jsx
@@ -2,7 +2,7 @@ import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "./ui/button";
-import { LayoutDashboard } from "lucide-react";
+import { LayoutDashboard, PenBox, PenBoxIcon } from "lucide-react";
 
 const Header = () => {
     return (
@@ -18,10 +18,17 @@ const Header = () => {
                 </Link>
                 <div>
                     <SignedIn>
-                        <Link href={"/dashboard"}>
+                        <Link href={"/dashboard"} className="text-gray-600 hover:text-blue-600 flex items-center gap-2">
                         <Button variant="outline">
                             <LayoutDashboard size={18}/>
                             <span className="hidden md:inline">Dashboard</span>
+                        </Button>
+                        </Link>
+
+                        <Link href={"/transaction/create"}>
+                        <Button className={"flex items-center gap-2"}>
+                            <PenBox size={18}/>
+                            <span className="hidden md:inline">Add Transaction</span>
                         </Button>
                         </Link>
                     </SignedIn>

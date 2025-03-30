c4eeb08ab20d0b2e363928ec13ba66cd02213d37
Kaushik Mandal <koushik369mondal@gmail.com>
2025-03-30 18:16:09 +0530

    enhance HeroSection with gradient styling and update dependencies

--- components/hero.jsx
+++ components/hero.jsx
@@ -7,8 +7,8 @@ import Image from "next/image";
 const HeroSection = () => {
     return (
         <div className="pb-20 px-4">
-            <div>
-                <h1>
+            <div className="container mx-auto text-center">
+                <h1 className="text-5xl md:text-8xl lg:text-[105px] pb-6 gradient-title">
                     Manage Your Finances <br /> with Intelligence
                 </h1>
                 <p>

0bdae81a511975a7a0190d06524a1a5d0a3fee18
Kaushik Mandal <koushik369mondal@gmail.com>
2025-07-23 22:42:43 +0530

    add Dashboard and MainLayout components; initialize account and transaction create pages

---
+++ app/(main)/layout.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function MainLayout({ children }) {
+    return (
+        <div className='container mx-auto my-32'>
+            {children}
+        </div>
+    )
+}

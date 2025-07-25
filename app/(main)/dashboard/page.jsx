a525c85f818b39bfc48b7b04f32287d3014f9b2f
Kaushik Mandal <koushik369mondal@gmail.com>
2025-07-25 17:14:35 +0530

    update DashboardPage layout and implement CreateAccountDrawer component

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,7 +1,19 @@
+import CreateAccountDrawer from '@/components/create-account-drawer'
 import React from 'react'
 
 export default function DashboardPage() {
     return (
-        <div className='px-5'>Page</div>
+        <div className='px-5'>
+            {/* Budget Progress  */}
+
+            {/* Overview  */}
+
+            {/* Accounts Grid  */}
+            <div>
+                <CreateAccountDrawer>
+                    open
+                </CreateAccountDrawer>
+            </div>
+        </div>
     )
 }

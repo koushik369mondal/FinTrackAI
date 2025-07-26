a902b45b94e0fd0cead0654e20f160683bcb9f8c
Kaushik Mandal <koushik369mondal@gmail.com>
2025-07-26 17:04:10 +0530

    Add account schema validation and enhance CreateAccountDrawer with form handling

--- components/create-account-drawer.jsx
+++ components/create-account-drawer.jsx
@@ -1,11 +1,38 @@
 "use client";
 
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-import { Drawer, DrawerContent, DrawerHeader, DrawerTitle, DrawerTrigger } from './ui/drawer';
+import {
+    Drawer,
+    DrawerContent,
+    DrawerHeader,
+    DrawerTitle,
+    DrawerTrigger,
+} from "./ui/drawer";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Input } from "./ui/input";
+import { accountSchema } from "../app/lib/schema";
 
-export default function CreateAccountDrawer({children}) {
+export default function CreateAccountDrawer({ children }) {
     const [open, setOpen] = useState(false);
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+        setValue,
+        watch,
+        reset,
+    } = useForm({
+        resolver: zodResolver(accountSchema),
+        defaultValues: {
+            name: "",
+            type: "CURRENT",
+            balance: "",
+            isDefault: false,
+        },
+    });
 
     return (
         <Drawer open={open} onOpenChange={setOpen}>
@@ -16,10 +43,20 @@ export default function CreateAccountDrawer({children}) {
                 </DrawerHeader>
                 <div>
                     <form action="">
-                        
+                        <div>
+                            <label htmlFor="name">Account Name</label>
+                            <Input
+                                id="name"
+                                placeholder="e.g., Main Checking"
+                                {...register("name")}
+                            />
+                            {errors.name && (
+                                <p className="text-sm text-red-500">{errors.name.message}</p>
+                            )}
+                        </div>
                     </form>
                 </div>
             </DrawerContent>
         </Drawer>
-    )
+    );
 }

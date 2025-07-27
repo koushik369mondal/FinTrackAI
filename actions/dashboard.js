cedddda61231bbbc8b5339c3f014e524ae85b556
Kaushik Mandal <koushik369mondal@gmail.com>
2025-07-27 14:01:05 +0530

    Refactor dashboard page and enhance account serialization logic

--- actions/dashboard.js
+++ actions/dashboard.js
@@ -5,12 +5,18 @@ import { auth } from "@clerk/nextjs/server";
 import { revalidatePath } from "next/cache";
 
 const serializeTransaction = (obj) => {
-    const serialized = {...obj };
+    const serialized = { ...obj };
 
-    if(obj.balance){
+    if (obj.balance) {
         serialized.balance = obj.balance.toNumber();
     }
-}
+
+    if (obj.amount) {
+        serialized.amount = obj.amount.toNumber();
+    }
+
+    return serialized;
+};
 
 export async function createAccount(data) {
     try {
@@ -59,8 +65,35 @@ export async function createAccount(data) {
         const serializedAccount = serializeTransaction(account);
 
         revalidatePath("/dashboard");
-        return {success: true, data: serializedAccount};
-    } catch (error) { 
+        return { success: true, data: serializedAccount };
+    } catch (error) {
         throw new Error(error.message);
     }
+}
+
+export async function getUserAccounts() {
+    const { userId } = await auth();
+    if (!userId) throw new Error("Unauthorized");
+
+    const user = await db.user.findUnique({
+        where: { clerkUserId: userId },
+    });
+
+    if (!user) {
+        throw new Error("User not found");
+    }
+
+    const accounts = await db.account.findMany({
+        where: { userId: user.id },
+        orderBy: { createdAt: "desc" },
+        include: {
+            _count: {
+                select: {
+                    transactions: true,
+                },
+            },
+        },
+    });
+    const serializedAccount = accounts.map(serializeTransaction);
+    return serializedAccount;
 }
--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,26 +1,36 @@
-import CreateAccountDrawer from '@/components/create-account-drawer'
-import { Card, CardContent } from '@/components/ui/card'
-import { Plus } from 'lucide-react'
-import React from 'react'
+import { getUserAccounts } from "@/actions/dashboard";
+import CreateAccountDrawer from "@/components/create-account-drawer";
+import { Card, CardContent } from "@/components/ui/card";
+import { Plus } from "lucide-react";
+import React from "react";
 
-export default function DashboardPage() {
+export default async function DashboardPage() {
+    const accounts = await getUserAccounts();
+
+    console.log("Accounts:", accounts);
+
     return (
-        <div className='px-5'>
+        <div className="px-5">
             {/* Budget Progress  */}
 
             {/* Overview  */}
 
             {/* Accounts Grid  */}
-            <div className='grid gap-4 md:grid-cols-2 lg:grid-cols-3'>
+            <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
                 <CreateAccountDrawer>
-                    <Card className='hover:shadow-md transition-shadow cursor-pointer border-dashed'>
-                        <CardContent className='flex flex-col items-center justify-center text-muted-foreground h-full pt-5'>
-                            <Plus className='h-10 w-10 mb-2'/>
-                            <p className='text-sm font-medium'>Add New Account</p>
+                    <Card className="hover:shadow-md transition-shadow cursor-pointer border-dashed">
+                        <CardContent className="flex flex-col items-center justify-center text-muted-foreground h-full pt-5">
+                            <Plus className="h-10 w-10 mb-2" />
+                            <p className="text-sm font-medium">Add New Account</p>
                         </CardContent>
                     </Card>
                 </CreateAccountDrawer>
+
+                {accounts.length > 0 &&
+                    accounts?.map((account) => {
+                        return account.name;
+                    })}
             </div>
         </div>
-    )
+    );
 }

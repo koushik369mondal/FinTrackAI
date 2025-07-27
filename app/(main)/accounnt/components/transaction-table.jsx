7841537275a5977078650e2ea5a3db3f936b4a14
Kaushik Mandal <koushik369mondal@gmail.com>
2025-07-27 22:29:08 +0530

    include sorting functionality and update table headers

--- app/(main)/accounnt/components/transaction-table.jsx
+++ app/(main)/accounnt/components/transaction-table.jsx
@@ -1,7 +1,12 @@
+"use client";
+
+import { Checkbox } from '@/components/ui/checkbox';
 import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
 import React from 'react'
 
 const TransactionTable = ({ transactions }) => {
+    const handleSort = () => { };
+
     return (
         <div className='space-y-4'>
             {/* Filters  */}
@@ -9,13 +14,30 @@ const TransactionTable = ({ transactions }) => {
             {/* Transactions   */}
             <div className='rounded-md border'>
                 <Table>
-                    <TableCaption>A list of your recent invoices.</TableCaption>
                     <TableHeader>
                         <TableRow>
-                            <TableHead className="w-[100px]">Invoice</TableHead>
-                            <TableHead>Status</TableHead>
-                            <TableHead>Method</TableHead>
-                            <TableHead className="text-right">Amount</TableHead>
+                            <TableHead className="w-[50px]">
+                                <Checkbox />
+                            </TableHead>
+                            <TableHead
+                                className="cursor-pointer"
+                                onClick={() => handleSort("date")}
+                            >
+                                <div className='flex items-center'>Date</div>
+                            </TableHead>
+                            <TableHead>Description</TableHead>
+                            <TableHead
+                                className="cursor-pointer"
+                                onClick={() => handleSort("category")}
+                            >
+                                <div className='flex items-center'>Category</div>
+                            </TableHead>
+                            <TableHead
+                                className="cursor-pointer"
+                                onClick={() => handleSort("amount")}
+                            >
+                                <div className='flex items-center justify-end'>Amount</div>
+                            </TableHead>
                         </TableRow>
                     </TableHeader>
                     <TableBody>

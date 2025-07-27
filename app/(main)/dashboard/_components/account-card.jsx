4253cb1184a79c5a7e013a949fd85f1288d470b0
Kaushik Mandal <koushik369mondal@gmail.com>
2025-07-27 15:03:51 +0530

    Add Income and Expense

--- app/(main)/dashboard/_components/account-card.jsx
+++ app/(main)/dashboard/_components/account-card.jsx
@@ -1,26 +1,40 @@
-import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
-import { Switch } from '@/components/ui/switch';
-import React from 'react'
+import {
+    Card,
+    CardContent,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
+import { Switch } from "@/components/ui/switch";
+import { ArrowDownRight, ArrowUpRight } from "lucide-react";
+import React from "react";
 
-export default function AccountCard({account}) {
-    const{name, type, balance, id, isDefault} = account;
+export default function AccountCard({ account }) {
+    const { name, type, balance, id, isDefault } = account;
     return (
         <Card>
             <CardHeader>
                 <CardTitle>{name}</CardTitle>
-                <Switch/>
+                <Switch />
             </CardHeader>
             <CardContent>
-                <div className='text-2xl font-bold'>
+                <div className="text-2xl font-bold">
                     ${parseFloat(balance).toFixed(2)}
                 </div>
-                <p className='text-xs text-muted-foreground capitalize'>
+                <p className="text-xs text-muted-foreground capitalize">
                     {type.charAt(0) + type.slice(1).toLowerCase()} Account
                 </p>
             </CardContent>
             <CardFooter>
-                <p>Card Footer</p>
+                <div className="flex items-center">
+                    <ArrowUpRight className="mr-1 h-4 w-4 text-green-500" />
+                    Income
+                </div>
+                <div className="flex items-center">
+                    <ArrowDownRight className="ml-7 mr-1 h-4 w-4 text-red-500" />
+                    Expense
+                </div>
             </CardFooter>
         </Card>
-    )
+    );
 }

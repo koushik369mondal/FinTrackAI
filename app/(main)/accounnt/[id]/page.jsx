faa641ae7285802f4bc9c70cc5ee3adcf3ff5bc0
Kaushik Mandal <koushik369mondal@gmail.com>
2025-07-27 17:51:13 +0530

    Refactor AccountDetailPage for improved layout and readability

--- app/(main)/accounnt/[id]/page.jsx
+++ app/(main)/accounnt/[id]/page.jsx
@@ -1,17 +1,36 @@
-import { getAccountWithTransactions } from '@/actions/accounts';
-import { notFound } from 'next/navigation';
-import React from 'react';
-import { get } from 'react-hook-form';
+import { getAccountWithTransactions } from "@/actions/accounts";
+import { notFound } from "next/navigation";
+import React from "react";
+import { get } from "react-hook-form";
 
 export default async function AccountDetailPage({ params }) {
     const accountData = await getAccountWithTransactions(params.id);
-    
-    if(!accountData){
+
+    if (!accountData) {
         notFound();
     }
+
+    const { transactions, ...account } = accountData;
+
     return (
-        <div className="">
-            {params.id}
+        <div className="space-y-8 px-5 flex gap-4 items-end justify-between">
+            <div className="">
+                <h1 className="text-5xl sm:text-6xl font-bold gradient-title capitalize">
+                    {account.name}
+                </h1>
+                <p className="text-muted-foreground">
+                    {account.type.charAt(0) + account.type.slice(1).toLowerCase()} Account
+                </p>
+            </div>
+
+            <div className="text-right pb-2">
+                <div className="text-xl sm:text-2xl font-bold">${parseFloat(account.balance).toFixed(2)}</div>
+                <p className="text-sm text-muted-foreground">{account._count.transactions} Transactions</p>
+            </div>
+
+            {/* Chart Section */}
+
+            {/* Transaction Table */}
         </div>
     );
 }

39c606bd1a64eec0ce55251b198a8b67390a3e25
Kaushik Mandal <koushik369mondal@gmail.com>
2025-08-07 09:12:45 +0530

    checkBudgetAlert for budget monitoring

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,12 +1,9 @@
 import { inngest } from "@/lib/inngest/client";
-import { helloWorld } from "@/lib/inngest/functions";
+import { checkBudgetAlert } from "@/lib/inngest/functions";
 import { serve } from "inngest/next";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
     client: inngest,
-    functions: [
-        /* your functions will be passed here later! */
-        helloWorld,
-    ],
+    functions: [checkBudgetAlert],
 });
